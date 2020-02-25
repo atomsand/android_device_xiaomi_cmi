d184529935681304fb9c9ba7459f8a41256e5e1d
Andy Chang <andychangzxx@outlook.com>
2020-02-25 17:00:43 +0800

    cmi: Initial tree
    Signed-off-by: Andy Chang <andychangzxx@outlook.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+Copyright (C) 2020 - The MoKee Open Source Project
+
+Device configuration for Xiaomi Mi 10 Pro (cmi)
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:----------
+CPU     | 1x 2.84GHz Kryo™ 585 + 3x 2.42GHz Kryo™ 585 + 4x 1.80Ghz Kryo™ 585 
+Chipset | Qualcomm® Snapdragon 865
+GPU     | Adreno 650
+ROM     | 256/512GB
+RAM     | 8/12GB
+Android | 10.0
+Battery | 4500 mAh
+Display | 2340x1080 pixels, 6.67 inch, 90Hz framerate
+Rear Camera  | 108/8/12/20MP
+Front Camera | 20MP
+
+## Device picture
+![Mi 10 Pro](https://cnbj1.fds.api.xiaomi.com/product-images/mi10pro/mi10pro-specs1.jpg "Mi 10 Pro")
---
+++ mokee_cmi.mk
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2020 The MoKee Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from cmi device
+$(call inherit-product, device/xiaomi/cmi/device.mk)
+
+# Inherit some common MoKee stuff.
+$(call inherit-product, vendor/mokee/config/common_full_phone.mk)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=atomsand
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := mokee_cmi
+PRODUCT_DEVICE := cmi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Xiaomi Mi 10 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "qti/qssi/qssi:10/QKQ1.191117.002/V11.0.2.0.QJACNXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="qssi-user 10 QKQ1.191117.002 V11.0.2.0.QJACNXM release-keys" \
+    PRODUCT_NAME="cmi" \
+    TARGET_DEVICE="cmi"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
