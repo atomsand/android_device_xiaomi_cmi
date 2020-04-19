599c818db1e6ad3cabf431642c883295dec00987
Demon000 <demonsingur@gmail.com>
2020-04-19 13:09:43 +0200

    cmi: Leave space for FOD while bouncer is requesting Pattern on PIN
    * SCREEN_HEIGHT (2340) - FOD_POS_Y (1808) + 18 = 550
    
    Change-Id: Iddfa076d92deaec982c5fe8711e155648da89965
    Signed-off-by: Andy Chang <andychangzxx@outlook.com>

--- device.mk
+++ device.mk
@@ -29,7 +29,8 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-mokee
 
 # Audio
 PRODUCT_COPY_FILES += \
